688a51a405e8aeb2086d5dd70f9171d4d97162ea
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2022-11-17 21:30:47 -0300

    :sparkles: feat: add scopes, shadowing, functions...

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,29 @@
+const PI:f32 = 3.14;
+static mut GLOBAL:u8 = 1;
+
+fn sum(x:i32, y:i32) -> i32 {
+  x + y // dont use ; and "return"
+}
+
+fn other_function() {
+  let age:i8 = 18;
+
+  {
+    let age:i8 = 30;
+
+    println!("Inside, age = {}", age);
+  }
+
+  println!("Outside, age = {}", age);
+}
+
 fn main() {
+  println!("PI = {}", PI);
+
+  unsafe {
+    println!("variavel global = {}", GLOBAL);
+  }
+
   println!("Hello, world!");
 
   let age:i8 = 14;
@@ -13,4 +38,8 @@ fn main() {
   letter = 'f';
 
   println!("Your letter is {} and the size of char is {} bytes", letter, std::mem::size_of_val(&letter));
+
+  other_function();
+
+  println!("Sum is {}", sum(1, 2));
 }
